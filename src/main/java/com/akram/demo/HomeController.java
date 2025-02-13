422d7ebe78a2f49c491452f8f06b043754ee2a4c
ziaulakram <mziaulakram@gmail.com>
2025-02-13 15:31:01 +0530

    first commit

---
+++ src/main/java/com/akram/demo/HomeController.java
@@ -0,0 +1,12 @@
+package com.akram.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping
+    public String home() {
+        return "Hello, World!";
+    }
+}
